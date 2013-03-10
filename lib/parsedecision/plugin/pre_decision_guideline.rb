b8422777e50b1ac2cc2cf72542b5a4e6f39ed237
Jeff McAffee <jeff@ktechsystems.com>
2013-03-10 15:29:53 -0700

    refactor pre_decision_guideline

--- lib/parsedecision/plugin/pre_decision_guideline.rb
+++ lib/parsedecision/plugin/pre_decision_guideline.rb
@@ -40,68 +40,110 @@ module Plugin
 
     def execute(context, ln)
       #$LOG.debug "PreDecisionGuideline::execute"
-      if((context.state == :app) && ln.include?(@searchStrPpms))
+
+      case context.state
+      when :app
+        return check_for_ppms_or_rule_start context, ln
+
+      when :preDecisionGdl
+        return process_rule_data context, ln
+
+      else
+        return false
+      end # case
+    end
+
+    def check_for_ppms_or_rule_start(context, ln)
+      if ln.include?(@searchStrPpms)
         @ppmData = ln
         # XML Tidy doesn't like underscores at the beginning attribute names, take care of it here.
         @ppmData.gsub!(/_DATA_SET/, "DATA_SET")
         @ppmData.gsub!(/_Name/, "Name")
         @ppmData.gsub!(/_Value/, "Value")
         return true
-      elsif((context.state == :app) && ln.include?(@searchStrGdl))
+      end
+
+      if ln.include?(@searchStrGdl)
         context.state = :preDecisionGdl
         @ruleData.clear
         @ruleData << "<!-- #{ln} -->"   # The leading element tag is not valid XML (no quotes around attrib params).
         return true
-      elsif((context.state == :preDecisionGdl) && ln.include?(@searchStrGdlEnd))
-        @ruleData << ln
-
-        # Default guideline name
-        gdlName = "PreDecision"
-
-        # String#match acts weird so using RegEx/MatchData here.
-        m = /\sGuidelineName="([^"]+)/.match(ln)
-        if m[1].length > 1
-          gdlName = m[1]
-          gdlName = context.createValidName(gdlName)
-        end
-
-        @outfile = apply_templates(@fnameTemplate, {"@INDEX@"=>context.indexStr, "@GDL@"=>gdlName})
-
-        # Store the closing tag for later.
-        @actualCloseTag = apply_template(@closeTag, "@TAG@", gdlName)
-
-        puts "Creating Gdl Rules file: #{@outfile}" if context.verbose
-
-        File.open(context.outputPath(@outfile), "w") do |f|
-          write_to_file(f, apply_template(@openTag, "@TAG@", gdlName))
-          write_to_file(f,@ppmData)
-        end
-        return true
-      elsif((context.state == :preDecisionGdl) && ln.include?(@searchRulesEnd))
-        @ruleData << ln
-
-        File.open(context.outputPath(@outfile), "a") do |f|
-          write_to_file(f,@ruleData)
-          write_to_file(f, @actualCloseTag)
-        end
-        context.state = :app
-        return true
-      elsif(context.state == :preDecisionGdl)
-        @ruleData << ln
-        @lineCount += 1
-
-        if(@lineCount > @chunkSize)
-          puts "Writing rule data chunk." if context.verbose
-          File.open(context.outputPath(@outfile), "a") do |f|
-            write_to_file(f,@ruleData)
-          end
-          @lineCount = 0
-          @ruleData.clear
-        end
-        return true
       end
+
       return false
     end
+
+    def process_rule_data(context, ln)
+      # Create the rules data file if the Guideline end tag is found
+      if ln.include?(@searchStrGdlEnd)
+        setup_rules_file context, ln
+        return true
+      end
+
+      # Close the rules data file if the rules end tag is found
+      if ln.include?(@searchRulesEnd)
+        close_out_rules_file context, ln
+        context.state = :app
+        return true
+      end
+
+      # Haven't found the start or end of the rules here,
+      # we must be somewhere in the middle.
+      # Store the data so it can be written to file later.
+      write_rules_data context, ln
+
+      return true
+    end
+
+    def setup_rules_file(context, ln)
+      @ruleData << ln
+
+      # Default guideline name
+      gdlName = "PreDecision"
+
+      # String#match acts weird so using RegEx/MatchData here.
+      m = /\sGuidelineName="([^"]+)/.match(ln)
+      if m[1].length > 1
+        gdlName = m[1]
+        gdlName = context.createValidName(gdlName)
+      end
+
+      @outfile = apply_templates(@fnameTemplate, {"@INDEX@"=>context.indexStr, "@GDL@"=>gdlName})
+
+      # Store the closing tag for later.
+      @actualCloseTag = apply_template(@closeTag, "@TAG@", gdlName)
+
+      puts "Creating Gdl Rules file: #{@outfile}" if context.verbose
+
+      File.open(context.outputPath(@outfile), "w") do |f|
+        write_to_file(f, apply_template(@openTag, "@TAG@", gdlName))
+        write_to_file(f,@ppmData)
+      end
+    end
+
+    def close_out_rules_file(context, ln)
+      @ruleData << ln
+
+      File.open(context.outputPath(@outfile), "a") do |f|
+        write_to_file(f,@ruleData)
+        write_to_file(f, @actualCloseTag)
+      end
+    end
+
+    def write_rules_data(context, ln)
+      @ruleData << ln
+      @lineCount += 1
+
+      if(@lineCount > @chunkSize)
+        puts "Writing rule data chunk." if context.verbose
+        File.open(context.outputPath(@outfile), "a") do |f|
+          write_to_file(f,@ruleData)
+        end
+        @lineCount = 0
+        @ruleData.clear
+      end
+    end
+    
   end # class PreDecisionGuideline
 
 
