4a761dde42c0363bf01cd1e165e868423ed95df2
Jeff McAffee <jeff@ktechsystems.com>
2013-03-10 16:25:25 -0700

    refactor product plugin

--- lib/parsedecision/plugin/product.rb
+++ lib/parsedecision/plugin/product.rb
@@ -48,11 +48,32 @@ module Plugin
     def execute(context, ln)
         #$LOG.debug "Product::execute"
 
-      if((context.state == :app) && ln.include?(@searchStr1))
+      case context.state
+      when :app
+        return accept_as_product_data context, ln
+
+      when :productXml
+        return accept_product_ppms context, ln
+
+      when :productPpms
+        return accept_product_rules context, ln
+
+      when :productRules
+        return process_product_rules context, ln
+
+      else
+        return false
+
+      end # case
+    end
+
+    def accept_as_product_data(context, ln)
+      if ln.include?(@searchStr1)
         context.state = :productXml
         @data.clear
         set_outfile( "" )
 
+        # Generate the name of the output file
         match = ln.match /\sName="([^"]+)/
         product = "UnkProduct"
         if(match && match.length > 1)
@@ -60,19 +81,32 @@ module Plugin
         end
         @product = context.createValidName(product)
         set_outfile( apply_templates(@fnameTemplate, {"@INDEX@"=>context.indexStr, "@PROD@"=>@product}) )
-        puts "Creating product file: #{@outfile}" if context.verbose
+
         @data << ln
-        File.open(context.outputPath(@outfile), "w") do |f|
-          write_to_file(f,@openTag)         # apply_template(@openTag, "@TAG@", context.createValidName(@product))
-          #debugger
-          write_to_file(f,context[:productXpath] ) if ! context[:productXpath].nil?
-          write_to_file(f,@data)
-        end
+
+        create_data_file context
+
         @data.clear
         return true
       end
 
-      if((context.state == :productXml) && ln.include?(@searchStr2))
+      return false
+    end
+
+    def create_data_file(context)
+      puts "Creating product file: #{@outfile}" if context.verbose
+      File.open(context.outputPath(@outfile), "w") do |f|
+        write_to_file(f,@openTag)
+
+        # Write the product PPM's Xpath (which was stored in the context)
+        write_to_file(f,context[:productXpath] ) if ! context[:productXpath].nil?
+
+        write_to_file(f,@data)
+      end
+    end
+
+    def accept_product_ppms(context, ln)
+      if ln.include?(@searchStr2)
         context.state = :productPpms
 
         # XML Tidy doesn't like underscores at the beginning attribute names, take care of it here.
@@ -83,48 +117,57 @@ module Plugin
         @data << ln
         return true
       end
+    end
 
-      if((context.state == :productPpms) && (ln.include?(@ruleStartStr) || ln.include?(@gdlStartStr)))
+    def accept_product_rules(context, ln)
+      if( ln.include?(@ruleStartStr) || ln.include?(@gdlStartStr) )
         context.state = :productRules
 
         @data << ln
         return true
       end
+    end
 
-      if((context.state == :productRules) && !ln.include?(@stopStr))
+    def process_product_rules(context, ln)
+      if !ln.include?(@stopStr)
         @data << ln
         @lineCount += 1
 
-        if(@lineCount > @chunkSize)
-          puts "Writing rule data chunk." if context.verbose
-          File.open(context.outputPath(@outfile), "a") do |f|
-            write_to_file(f,@data)
-          end
-          @lineCount = 0
-          @data.clear
-        end
+        write_data_chunk context
+
         return true
-      end
+      else
 
-      if((context.state == :productRules) && ln.include?(@stopStr))
         @data << ln
         @lineCount += 1
 
-        puts "Closing product file #{@outfile}." if context.verbose
-        File.open(context.outputPath(@outfile), "a") do |f|
-          write_to_file(f,@data)
-          write_to_file(f,@closeTag)      # apply_template(@closeTag, "@TAG@", context.createValidName(@product))
-        end
-        @lineCount = 0
-        @data.clear
+        close_out_data_file context
         context.state = :app
 
         return true
       end
-
-      return false
     end
 
+    def write_data_chunk(context)
+      if(@lineCount > @chunkSize)
+        puts "Writing rule data chunk." if context.verbose
+        File.open(context.outputPath(@outfile), "a") do |f|
+          write_to_file(f,@data)
+        end
+        @lineCount = 0
+        @data.clear
+      end
+    end
+
+    def close_out_data_file(context)
+      puts "Closing product file #{@outfile}." if context.verbose
+      File.open(context.outputPath(@outfile), "a") do |f|
+        write_to_file(f,@data)
+        write_to_file(f,@closeTag)      # apply_template(@closeTag, "@TAG@", context.createValidName(@product))
+      end
+      @lineCount = 0
+      @data.clear
+    end
 
   end # class Product
 
