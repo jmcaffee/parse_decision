ae2b7270cd45b2a63d6be7adf263d5da43910c33
Jeff McAffee <jeff@ktechsystems.com>
2015-08-14 11:54:31 -0700

    Update rake tasks
    * Remove RDoc task
    * Add RSpec task (spec)

--- rakefile.rb
+++ rakefile.rb
@@ -8,51 +8,20 @@
 ######################################################################################
 
 require 'bundler/gem_tasks'
-require 'psych'
-gem 'rdoc', '>= 3.9.4'
+#require 'psych'
 
 require 'rake'
 require 'rake/clean'
-require 'rdoc/task'
-
+require 'rspec/core/rake_task'
 
 # Set the project name
 PROJNAME        = "ParseDecision"
 
 
-#############################################################################
-RDoc::Task.new(:rdoc) do |rdoc|
-    files = ['README.rdoc', 'docs/**/*.rdoc', 'lib/**/*.rb', 'bin/**/*']
-    rdoc.rdoc_files.add( files )
-    # Page to start on
-    rdoc.main = "README.md"
-  #puts "PWD: #{FileUtils.pwd}"
-    rdoc.title = "#{PROJNAME} Documentation"
-    rdoc.rdoc_dir = 'doc'                   # rdoc output folder
-    rdoc.options << '--line-numbers' << '--all'
-end
-
-
 
 #############################################################################
-desc "Run all tests"
-task :test => [:init] do
-  unless File.directory?('test')
-    $stderr.puts 'no test in this package'
-    return
-  end
-  $stderr.puts 'Running tests...'
-  TESTDIR = "test"
-  begin
-    require 'test/unit'
-  rescue LoadError
-    $stderr.puts 'test/unit cannot loaded.  You need Ruby 1.8 or later to invoke this task.'
-  end
-
-  $LOAD_PATH.unshift("./")
-  $LOAD_PATH.unshift(TESTDIR)
-  Dir[File.join(TESTDIR, "*.rb")].each {|file| require File.basename(file) }
-  require 'minitest/autorun'
+desc "Run all specs"
+RSpec::Core::RakeTask.new do |t|
+  #t.rcov = true
 end
-
 
