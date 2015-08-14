03b2ac2f47426d5efa0b3cc5490ce2bb5697c969
Jeff McAffee <jeff@ktechsystems.com>
2015-08-14 11:55:52 -0700

    Bump version to 0.0.6

--- lib/parse_decision/version.rb
+++ lib/parse_decision/version.rb
@@ -1,5 +1,5 @@
 module ParseDecision
-  VERSION = "0.0.5" unless constants.include?("VERSION")
+  VERSION = "0.0.6" unless constants.include?("VERSION")
   APPNAME = "ParseDecision" unless constants.include?("APPNAME")
   COPYRIGHT = "Copyright (c) 2014, kTech Systems LLC. All rights reserved" unless constants.include?("COPYRIGHT")
 end
